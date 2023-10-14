2db18d4c85b1507cdebda94ecd7df36b2d1eae8d
drewpyun <drew.pyun@gmail.com>
2023-10-13 23:46:52 -0400

    updated comments

--- unencrypted/client.py
+++ unencrypted/client.py
@@ -7,74 +7,110 @@ import wave
 import time
 from queue import Queue
 
+# Function to get available input devices
 def get_input_devices():
     p = pyaudio.PyAudio()
+    # Collect the device information only if maxInputChannels > 0 (i.e., it can record audio).
     devices = [{"index": i, "name": p.get_device_info_by_index(i)["name"]} for i in range(p.get_device_count()) if p.get_device_info_by_index(i).get('maxInputChannels') > 0]
-    p.terminate()
+    p.terminate()  # Close the PyAudio stream
     return devices
 
+# Function to receive audio data from the server and play it back
 def receive_data(conn, volume_multiplier, audio_queue):
     p = pyaudio.PyAudio()
+    # Initialize a new audio stream to play received audio.
     stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, output=True, frames_per_buffer=1024)
     print("Receiving audio...")
     while True:
+        # Receive data from the server
         data = conn.recv(1024)
+
+        # Break the loop if no data is received.
         if not data:
             break
+
+        # Create an empty bytearray to store adjusted audio data.
         adjusted_data = bytearray()
+
         for i in range(0, len(data), 2):
+            # Extract each audio sample, which is 2 bytes long.
             sample = int.from_bytes(data[i:i+2], byteorder='little', signed=True)
+
+            # Adjust the audio sample by the volume_multiplier.
             adjusted_sample = int(sample * volume_multiplier)
+
+            # Append the adjusted sample to the adjusted_data array.
             adjusted_data.extend(adjusted_sample.to_bytes(2, byteorder='little', signed=True))
+
+        # Write the adjusted audio data to the audio stream for playback.    
         stream.write(bytes(adjusted_data))  
+
+        # Add the adjusted audio data to the queue for potential saving.
         audio_queue.put(bytes(adjusted_data))
+
+    # Cleanup after loop ends.    
     stream.stop_stream()
     stream.close()
     p.terminate()
     print("Stopped receiving audio.")
 
+# Function to ensure that we receive the complete data payload.
 def receive_complete_data(sock, expected_length):
     received_data = b""
     while len(received_data) < expected_length:
+        # Calculate how much more data needs to be received.
         remaining_length = expected_length - len(received_data)
+
+        # Receive the remaining data or 1024 bytes, whichever is smaller.
         new_data = sock.recv(min(remaining_length, 1024))
+
+        # If no new data is received, consider the connection as broken and raise an exception.
         if not new_data:
             raise ConnectionError("Socket connection broken")
+        
+        # Append the newly received data to the existing data.
         received_data += new_data
     return received_data.decode('utf-8')
 
+# Function to save audio data as a .wav file
 def save_audio_file(frames):
     if not frames:
         return
     timestamp = time.strftime("%Y%m%d%H%M%S")
     filename = f"recorded_audio_{timestamp}.wav"
+
+    # Create a new .wav file and write frames into it.
     with wave.open(filename, 'wb') as wf:
         wf.setnchannels(1)
         wf.setsampwidth(2)
         wf.setframerate(44100)
         wf.writeframes(b''.join(frames))
+
     print(f"Audio saved as {filename}")
 
+# Main function where the program execution starts
 def main():
-    try:
+    try:  # Attempt to read previously used database IPs from a file.
         with open('user-settings.txt', 'r') as f:
             database_ips = f.read().splitlines()
-    except FileNotFoundError:
+    except FileNotFoundError:  # If the file doesn't exist, initialize an empty list.
         database_ips = []
 
     print("Stored database IPs:")
     for i, ip in enumerate(database_ips):
         print(f"{i+1}. {ip}")
 
+    # User interface to select or input a new database IP.
     choice = input("Enter the number of a stored database IP or 'n' to enter a new one: ")
     if choice.lower() == 'n':
         database_ip = input("Enter the IP of the database server: ")
+        # Save the new IP into the user-settings file for future use.
         with open('user-settings.txt', 'a') as f:
             f.write(f"{database_ip}\n")
-    else:
+    else:  # Use the previously stored IP as selected by the user.
         database_ip = database_ips[int(choice) - 1]
 
-    try:
+    try:  # Fetch the list of available server IPs from the database server.
         response = requests.get(f"http://{database_ip}:8000/get_ips")
         available_server_ips = response.json()['ips']
     except requests.exceptions.JSONDecodeError:
@@ -85,20 +121,26 @@ def main():
     for i, ip in enumerate(available_server_ips):
         print(f"{i+1}. {ip}")
 
+    # User interface to select or input a new server IP.
     server_choice = input("Enter the number of the server IP you want to connect to or 'm' for manual entry: ")
     if server_choice.lower() == 'm':
         HOST = input("Enter the IP of the server: ")
-    else:
+    else:   # Use the selected available server IP.
         HOST = available_server_ips[int(server_choice) - 1]
 
-    PORT = 65432
+    PORT = 65432  # The port on which the server is running.
+
+    # Ask for a volume multiplier from the user.
     volume_multiplier = float(input("Enter the volume multiplier (e.g., 0.5 for lower volume): "))
 
+    # Initialize a Queue to hold audio frames for possible saving.
     audio_queue = Queue()
 
+    # Create a new socket and connect to the server.
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
 
+        # Fetch and parse the list of available input devices from the server.
         length_str = s.recv(4).decode('utf-8')
         expected_length = int(length_str)
 
@@ -108,13 +150,18 @@ def main():
         for i, device in enumerate(device_data):
             print(f"{i+1}. {device['name']}")
 
+        # User interface to select an input device.
         device_choice = int(input("Choose an input device: "))
         device_index = device_data[device_choice - 1]['index']
+
+        # Send the selected device index back to the server.
         s.sendall(str(device_index).encode('utf-8'))
 
+        # Start the thread that receives and plays audio.
         t = threading.Thread(target=receive_data, args=(s, volume_multiplier, audio_queue))
         t.start()
 
+        # User interface for stopping the audio or saving it.
         while True:
             command = input("Enter 'stop' to stop or 'save' to save audio: ")
             if command == 'stop':
@@ -128,4 +175,4 @@ def main():
                 save_audio_file(frames_to_save)
 
 if __name__ == "__main__":
-    main()
+    main()  # Main program
--- unencrypted/database.py
+++ unencrypted/database.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 import psutil
 import socket
 
+# Function to get available network interfaces on the server
 def get_network_interfaces():
     interfaces = psutil.net_if_addrs()
     available_ips = []
@@ -11,32 +12,36 @@ def get_network_interfaces():
                 available_ips.append((interface, addr.address))
     return available_ips
 
-# Get available IPs
+# Call the function to get available IPs and display them to the user
 available_ips = get_network_interfaces()
 print("Available Network Interfaces and IP addresses:")
 for i, (interface, ip) in enumerate(available_ips):
     print(f"{i+1}. {interface} - {ip}")
 
-# User chooses the IP
+# User selects an IP address to use for the Flask application
 ip_choice = int(input("Enter the number of the IP you want to use: "))
 chosen_ip = available_ips[ip_choice - 1][1]
 
+# Initialize Flask app
 app = Flask(__name__)
-stored_ips = []  # List to store the IPs
+stored_ips = []  # List to keep track of stored IP addresses
 
+# Define route to add new IP to the list of stored IPs
 @app.route('/add/<ip>', methods=['POST'])
 def add_ip(ip):
-    global stored_ips
+    global stored_ips  # Use the global variable to manage IPs
     if ip not in stored_ips:
         stored_ips.append(ip)
-    print(f"Stored IPs: {stored_ips}")  # Debug line to print stored IPs
-    return jsonify({'result': 'success'}), 200
+    print(f"Stored IPs: {stored_ips}")  # Debug line to print IPs that are stored
+    return jsonify({'result': 'success'}), 200  # Return success as JSON
 
+# Define route to get the list of stored IPs
 @app.route('/get_ips', methods=['GET'])
 def get_ips():
-    global stored_ips
-    print(f"Sending IPs: {stored_ips}")  # Debug line to print sent IPs
-    return jsonify({'ips': stored_ips}), 200
+    global stored_ips  # Use the global variable to manage IPs
+    print(f"Sending IPs: {stored_ips}")  # Debug line to print IPs that are sent
+    return jsonify({'ips': stored_ips}), 200  # Return list of IPs as JSON
 
+# Start the Flask app on the chosen IP and port 8000
 if __name__ == '__main__':
     app.run(host=chosen_ip, port=8000)
